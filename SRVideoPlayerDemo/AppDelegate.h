d48174769ceb0bb693ecd7c35db353545c754de6
潘神 <415391360@qq.com>
2018-11-30 16:48:57 +0800

    Initial Commit

---
+++ SRVideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SRVideoPlayerDemo
+//
+//  Created by longrise on 2018/11/30.
+//  Copyright © 2018 longrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SRVideoPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SRVideoPlayerDemo
+//
+//  Created by longrise on 2018/11/30.
+//  Copyright © 2018 longrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
